c2113168b379b563d607343beb216c09b39a91a8
Code Nation <codenation@Codes-MacBook-Pro.local>
2020-02-20 16:41:28 +0000

    css

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const express = require('express');
 const bodyParser = require('body-parser');  //import body-parser
 const app = express();
 
- //import the getWeather function
- //using the fetch method 
+//import the getWeather function
+//using the fetch method 
+//requireing the file
 const fetchWeather = require('./lib/fetchWeather');
 // this is with require. it is depricated now -  start usimng fetch instead
 // const getWeather = require('./lib/getWeather');
@@ -17,8 +18,8 @@ const fetchWeather = require('./lib/fetchWeather');
 // i,mages, css, javascript
 app.use(express.static(path.join(__dirname, 'public')));
 // console.log(__dirname)    ///Users/codenation/Desktop/node/wheaterFront
-app.use(bodyParser.urlencoded({extended: false})); //Ignore data types and make everything a string
-app.use(bodyParser.json());  // Parse data as JSON
+app.use(bodyParser.urlencoded({ extended: false })); //Ignore data types and make everything a string
+app.use(bodyParser.json());  // Parse data as JSON 
 
 
 // to keep every pagae consistant, don't have to duplicate code
@@ -29,6 +30,7 @@ app.engine('.hbs', hbs({
 }))
 
 // telling the express to use the engine
+// we are ready to use this
 app.set('view engine', '.hbs');
 
 
@@ -37,31 +39,32 @@ app.get('/', (req, res) => {
 })
 
 // POST route for “/” that is being used in the form
-app.post('/', async(req, res) => {
-    let location  = req.body.location;   //Get the location from the requests body object
+//async - wait to run the getweather function
+app.post('/', async (req, res) => {
+    let location = req.body.location;   //Get the location from the requests body object
     let countryCode = req.body.countryCode
-    console.log(location)
+    // console.log(location)
     let data = await fetchWeather(location, countryCode);
 
     console.log(data)
     if (data.cod !== '404') {
 
-    let description = data.weather[0].description
-    let locationName = data.name + ', '
-    let countryCodeName = data.sys.country
-    let temp = data.main.temp + ' C'
-    let feelsLike = data.main.feels_like + ' C'
-    let iconData = data.weather[0].icon
-    let iconUrl = `http://openweathermap.org/img/wn/${iconData}@2x.png`
-    let windSpeed = data.wind.speed + ' m/s'
-    console.log(data)
-    // render the index.hbs page
-    // Send an object inside an object so that we can loop through the data
-    //locationName, countryCodeName - I want to disply in the h1, not in the list, so it has to be outside the obdjec
-    res.render('index', {data: {Temperature: temp, Feels: feelsLike, Wind: windSpeed, Description: description, }, iconUrl, locationName, countryCodeName});
+        let description = data.weather[0].description
+        let locationName = data.name + ', '
+        let countryCodeName = data.sys.country
+        let temp = data.main.temp + ' C'
+        let feelsLike = data.main.feels_like + ' C'
+        let iconData = data.weather[0].icon
+        let iconUrl = `http://openweathermap.org/img/wn/${iconData}@2x.png`
+        let windSpeed = data.wind.speed + ' m/s'
+        console.log(data)
+        // render the index.hbs page
+        // Send an object inside an object so that we can loop through the data
+        //locationName, countryCodeName - I want to disply in the h1, not in the list, so it has to be outside the obdjec
+        res.render('index', { data: { Temperature: temp, Feels: feelsLike, Wind: windSpeed, Description: description, }, iconUrl, locationName, countryCodeName });
 
     } else {
-        res.render('index', {err: "The location you entered dosen't exist!"})
+        res.render('index', { err: "The location you entered dosen't exist!" })
     }
 
 })
